a796cf43f9baf66f66033bf781cbc113388fff32
neoapps-dev <asd22.info@gmail.com>
2026-02-21 20:46:19 +0300

    .

--- null/basefs/manifest.lua
+++ null/basefs/manifest.lua
@@ -17,24 +17,36 @@ pkg.sources = {
 	},
 }
 
+function install()
+	local path = CONFIG.TEMP_INSTALL_PATH .. "/" .. pkg.name
+	exec(
+		"cd "
+			.. path
+			.. " && mkdir -p usr/bin usr/lib usr/lib64 home var/spool/main var/www root proc sys var/cache var/db var/tmp srv boot mnt opt media run etc"
+	)
+	exec("cd " .. path .. " && ln -s usr/bin bin")
+	exec("cd " .. path .. " && ln -s usr/bin sbin")
+	exec("cd " .. path .. " && ln -s usr/lib lib")
+	exec("cd " .. path .. " && ln -s usr/lib64 lib64")
+	exec("cd " .. path .. " && ln -s usr/bin usr/sbin")
+	exec("cd " .. path .. " && ln -s var/tmp tmp")
+	install({ "passwd", "-m 644", "--target-directory=" .. path .. "/etc/" })
+	install({ "shadow", "-m 644", "--target-directory=" .. path .. "/etc/" })
+	install({ "group", "-m 644", "--target-directory=" .. path .. "/etc/" })
+end
+
 function pkg.binary()
 	return function(hook)
 		hook("install")(function()
-			local path = CONFIG.TEMP_INSTALL_PATH .. "/" .. pkg.name
-			exec(
-				"cd "
-					.. path
-					.. " && mkdir -p usr/bin usr/lib usr/lib64 home var/spool/main var/www root proc sys var/cache var/db var/tmp srv boot mnt opt media run etc"
-			)
-			exec("cd " .. path .. " && ln -s usr/bin bin")
-			exec("cd " .. path .. " && ln -s usr/bin sbin")
-			exec("cd " .. path .. " && ln -s usr/lib lib")
-			exec("cd " .. path .. " && ln -s usr/lib64 lib64")
-			exec("cd " .. path .. " && ln -s usr/bin usr/sbin")
-			exec("cd " .. path .. " && ln -s var/tmp tmp")
-			install({ "passwd", "-m 644", "--target-directory=" .. path .. "/etc/" })
-			install({ "shadow", "-m 644", "--target-directory=" .. path .. "/etc/" })
-			install({ "group", "-m 644", "--target-directory=" .. path .. "/etc/" })
+			install()
+		end)
+	end
+end
+
+function pkg.source()
+	return function(hook)
+		hook("install")(function()
+			install()
 		end)
 	end
 end
