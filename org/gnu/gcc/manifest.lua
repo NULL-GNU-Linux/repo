c36333e2b237df7d447736688bd183ca221ecf8f
neoapps-dev <asd22.info@gmail.com>
2026-02-28 01:03:16 +0300

    h

---
+++ org/gnu/gcc/manifest.lua
@@ -0,0 +1,65 @@
+pkg = {
+	name = "org.gnu.gcc",
+	version = "15.2.0",
+	description = "The GNU Compiler Collection",
+	maintainer = "NEOAPPS <neo@obsidianos.xyz>",
+	license = "GPL-3.0",
+	homepage = "https://gcc.gnu.org",
+	depends = {},
+	conflicts = { },
+	provides = { "gcc", "g++", "ada", "fortran", "go", "objc", "c++", "c", "obj-c++", "m2" },
+	options = {
+		config = {
+			type = "string",
+			default = "",
+			description = "extra configuration options to pass to ./configure",
+		}
+	},
+}
+pkg.sources = {
+	source = {
+		type = "tar",
+		url = "https://mirrors.dotsrc.org/gnu/gcc/gcc-"..pkg.version.."/gcc-"..pkg.version.."/.tar.xz",
+		args = "--strip-components=1",
+	},
+	binary = {
+		type = "tar",
+		url = "https://files.obsidianos.xyz/~neo/null/" .. ARCH .. "-gcc.tar.gz",
+	},
+}
+function pkg.source()
+	return function(hook)
+		hook("prepare")(function()
+            exec("sed -i 's/char [*]q/const &/' libgomp/affinity-fmt.c") -- for glibc >=2.43
+			local configure_opts = {
+				"--prefix=/usr",
+                "--disable-multilib",
+                "--with-system-zlib",
+                "--enable-default-pie",
+                "--enable-default-ssp",
+                "--enable-host-pie",
+                "--disable-fixincludes",
+                "--enable-languages=c,c++,fortran,go,objc,obj-c++,m2"
+			}
+			local configg = OPTIONS.config or ""
+			table.insert(configure_opts, configg)
+			configure(configure_opts, "configure || mkdir -p build && cd build && ./configure ")
+		end)
+
+		hook("build")(function()
+            make()
+		end)
+
+		hook("install")(function()
+			make({}, false)
+		end)
+	end
+end
+
+function pkg.binary()
+	return function(hook)
+		hook("install")(function()
+			install({ "*", INSTALL }, "cp -r")
+		end)
+	end
+end
