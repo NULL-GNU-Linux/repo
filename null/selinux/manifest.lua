8d6dd9c7739623213a2162c299aacd5e9b1b752f
neoapps-dev <asd22.info@gmail.com>
2026-03-22 23:53:13 +0300

    init

---
+++ null/selinux/manifest.lua
@@ -0,0 +1,41 @@
+pkg = {
+	name = "null.selinux",
+	version = "3.10",
+	description = "A flexible Mandatory Access Control system built into the Linux Kernel",
+	maintainer = "NEOAPPS <neo@obsidianos.xyz>",
+	license = "GPL-2.0",
+	homepage = "https://github.com/SELinuxProject/selinux",
+	depends = {},
+	conflicts = {},
+	provides = { "libselinux", "selinux" },
+}
+pkg.sources = {
+	source = {
+		type = "tar",
+		url = "https://github.com/SELinuxProject/selinux/archive/refs/tags/"..pkg.version..".tar.gz",
+		args = "--strip-components=1",
+	},
+	binary = {
+		type = "tar",
+		url = "https://files.obsidianos.xyz/~neo/null/" .. ARCH .. "-selinux.tar.gz",
+	},
+}
+function pkg.source()
+	return function(hook)
+		hook("build")(function()
+            make()
+		end)
+
+		hook("install")(function()
+			make({}, false)
+		end)
+	end
+end
+
+function pkg.binary()
+	return function(hook)
+		hook("install")(function()
+			install({ "*", INSTALL }, "cp -r")
+		end)
+	end
+end
